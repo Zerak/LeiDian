0437dd25d9a1d7899683683419ea16ae5d72dc22
Zerak <Kingbug2010@126.com>
2014-05-21 22:01:30 +0800

    config adroid.mk

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -7,7 +7,29 @@ LOCAL_MODULE := cocos2dcpp_shared
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/*.cpp
+                   ../../Classes/UnitSprite.cpp \
+                   ../../Classes/RoleSprite.cpp \
+                   ../../Classes/PlayerSprite.cpp \
+                   ../../Classes/MoveTrackArray.cpp \
+                   ../../Classes/LoadLogo.cpp \
+                   ../../Classes/ItemSprite.cpp \
+                   ../../Classes/ItemManager.cpp \
+                   ../../Classes/HelloWorldScene.cpp \
+                   ../../Classes/GameOver.cpp \
+                   ../../Classes/GameMessage.cpp \
+                   ../../Classes/GameMenu.cpp \
+                   ../../Classes/GameMap.cpp \
+                   ../../Classes/GameManager.cpp \
+                   ../../Classes/GameLevel.cpp \
+                   ../../Classes/GameEvents.cpp \
+                   ../../Classes/EnemySprite.cpp \
+                   ../../Classes/EnemyManager.cpp \
+                   ../../Classes/Effect.cpp \
+                   ../../Classes/CCRadioMenu.cpp \
+                   ../../Classes/BulletSprite.cpp \
+                   ../../Classes/BulletManager.cpp \
+                   ../../Classes/BossSprite.cpp \
+                   ../../Classes/AppDelegate.cpp \
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
